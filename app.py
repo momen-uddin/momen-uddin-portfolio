e2a75b09c995b235de0f71c7e6b5daef157b4f07
momen-uddin <momen4@dti.ac>
2024-11-06 22:02:34 +0600

    Add contact form functionality and update HTML structure

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        clientName = request.form['name']
+        clientEmail = request.form['email']
+        clientPhone = request.form['phone']
+        clientMsg = request.form['msg']
+        
+        return render_template("index.html")
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
